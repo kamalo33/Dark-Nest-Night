22704b2e05f43b927ef6077f769e27386387f5a5
ElriKong <45388311+ElriKong@users.noreply.github.com>
2019-04-23 13:29:23 +0200

    Cazador

---
+++ Assets/Scripts/Hunter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunter : MonoBehaviour {
+
+    bool ifTocado;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ifTocado == true)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * 2);
+            Invoke("Stop", 2);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ifTocado = true;
+        }
+        
+    }
+
+    private void Stop()
+    {
+        Destroy(this.gameObject);
+    }
+
+ 
+}
